115897a960341563a26e6292a24a21e03946b956
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-07-19 20:53:38 +0800

    Squashed main into combatMaps
    le zgho

---
+++ Tech_Demo_Orbital_Y22W19/Assets/Scripts/Behaviours/ApplicationQuitBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuitBehaviour : MonoBehaviour
+{
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
